10d53152c8c528397b3110fa3a7c4fdaa625545a
Sayak Brahmachari <sayak-brm@users.noreply.github.com>
2021-08-01 13:13:40 +0530

    Add SparseMatrix

---
+++ src/rpi/sparse_matrix/__init__.py
@@ -0,0 +1,50 @@
+class SparseMatrix:
+    """
+    Sparse matrix data structure with support for negative indexes.
+    Attributes:
+        head (dict): Dictionary containing the sparse matrix.
+        default (any): Default value of the sparse matrix.
+    """
+
+    def __init__(self, default=0, head={}):
+        """
+        Initialize the sparse matrix.
+        Args:
+            default (any, optional): Default value of the sparse matrix. Defaults to 0.
+            head (dict, optional): Dictionary containing the sparse matrix. Defaults to {}.
+        """
+        self.head = head
+        self.default = default
+
+    def update(self, x, y, val):
+        """
+        Update a position in the sparse matrix.
+        Args:
+            x (int): Index of the top-level dict.
+            y (int): Index of the nested dict.
+            val (any): New value of the position to be updated.
+        """
+        if val == self.default:
+            if x in self.head and y in self.head[x]:
+                del self.head[x][y]
+                if self.head[x] == {}:
+                    del self.head[x]
+        else:
+            if x in self.head:
+                self.head[x][y] = val
+            else:
+                self.head[x] = {y: val}
+
+    def get(self, x, y):
+        """
+        Get a value from a particular position.
+        Args:
+            x (int): Index of top-level dict.
+            y (int): Index of nested dict.
+        Returns:
+            [type]: [description]
+        """
+        if x in self.head and y in self.head[x]:
+            return self.head[x][y]
+        else:
+            return self.default
